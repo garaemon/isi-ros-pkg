663aa46722ed8ca7516789c9fd89fdcc39040a31
Ryohei Ueda <garaemon@gmail.com>
2014-05-27 13:59:31 +0900

    add catkin_package macro

--- isi_3d_recognition/c3_hlac_core/catkin.cmake
+++ isi_3d_recognition/c3_hlac_core/catkin.cmake
@@ -6,4 +6,12 @@ find_package(catkin REQUIRED COMPONENTS roscpp)
 # actually we should run 
 execute_process(COMMAND cmake -E chdir ${PROJECT_SOURCE_DIR} make -f Makefile.download)
 
+catkin_package(
+    DEPENDS 
+    CATKIN_DEPENDS roscpp
+    INCLUDE_DIRS include
+    LIBRARIES 
+)
+
+
 install(FILES lib/libc3_hlac_core.so DESTINATION ${CATKIN_PACKAGE_LIB_DESTINATION})
